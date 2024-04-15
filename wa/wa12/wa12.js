95ca3ed5a886f6616500219da47cd3f8c63fdad4
mudhaw1 <Mudhawi.Algannas@colorado.edu>
2024-04-14 18:54:09 -0600

    wa12

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -6,26 +6,26 @@ ansBtn.addEventListener('click', getAnswer);
 
 const answerText = document.querySelector("#js-answer-text");
 
-const endpt = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
+const endpt = 'https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=boolean';
 
 let answer= '';
 
 
 async function getQuote(){
-
+    
     try {
-        const response = await fetch(endpt);
+    const response = await fetch(endpt);
         if(!response.ok){
             throw Error(response.statusText)
         }
-    
+
     const json = await response.json();
     displayQuote(json['question']);
-    answer = json['answer'];
+    answer = json['correct_answer'];
     answerText.textContent = '';
 
     } catch (err) {
-       console.log(err);
+        console.log(err);
        alert('Failed to fetch new quote');
     }
 }
