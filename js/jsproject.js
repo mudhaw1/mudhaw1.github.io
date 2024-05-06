a66705462bc0bb4f6d866cad5cf8b6affc456755
mudhaw1 <Mudhawi.Algannas@colorado.edu>
2024-05-05 20:21:33 -0600

    jsproject

--- js/jsproject.js
+++ js/jsproject.js
@@ -1,6 +1,7 @@
 const volumeBar = document.getElementById('volumeBar');
 const overlay = document.getElementById('overlay');
 const subscriptionPopup = document.getElementById('subscriptionPopup');
+const closeBtn = document.getElementsByClassName('close');
 
 volumeBar.addEventListener('mousedown', function(event) {
   event.preventDefault();
@@ -17,5 +18,23 @@ function buySubscription(plan) {
     showPopup("To access this feature, please buy the monthly subscription for $2.99.");
   } else if (plan === '3months') {
     showPopup("To access this feature, please buy the 3-months subscription for $3.99.");
+  }
+}
+function closePopup() {
+  overlay.style.display = 'none';
+  generateTabs();
+}
+
+function generateTabs() {
+  const body = document.body;
+  const tabContent = ["Please try again", "Please try once more", "Retry", "Give it another shot", "Please give another attempt"];
+
+  while (true) {
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.style.left = `${Math.random() * 90}%`;
+    tab.style.top = `${Math.random() * 90}%`;
+    tab.textContent = tabContent[Math.floor(Math.random() * tabContent.length)];
+    body.appendChild(tab);
   }
 }
